ae6b09aba7e70a29d334bf94aa123b277a669182
Josh kelley <jkelleypga@gmail.com>
2018-05-10 10:38:43 -0500

    updating main py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -12,9 +12,9 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     """ Whenever an issue is opened, greet the author and say thanks."""
     url = event.data["issue"]["comments_url"]
     author = event.data["issue"]["user"]["login"]
-	message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
-	await gh.post(url, data={"body": message})
-	
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
 async def main(request):
     # read the GitHub webhook payload
     body = await request.read()
